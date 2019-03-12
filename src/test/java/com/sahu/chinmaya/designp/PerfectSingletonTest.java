1e225f94ed84840c5654660c87bb27ffcb076a86
chinmaya <chinmayasa@gmail.com>
2019-03-13 01:53:03 +0530

    changed the test class

--- src/test/java/com/sahu/chinmaya/designp/PerfectSingletonTest.java
+++ src/test/java/com/sahu/chinmaya/designp/PerfectSingletonTest.java
@@ -3,12 +3,18 @@ package com.sahu.chinmaya.designp;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class SingletonTest {
+public class PerfectSingletonTest {
 
 	@Test
 	public void testSingleTon()
 	{
 		PerfectSingleton perfectSingleton = PerfectSingleton.getInstance();
+		try {
+			Thread.sleep(1000L);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		PerfectSingleton perfectSingleton2 = PerfectSingleton.getInstance();
 		Assert.assertEquals("Equal",perfectSingleton.getName(), perfectSingleton2.getName());
 		
