397393b3bddb99e33c54789a62b00e14ad39aea2
chinmaya <chinmayasa@gmail.com>
2019-03-13 01:42:35 +0530

    formatted

--- src/main/java/com/sahu/chinmaya/misc/SerilizationTest.java
+++ src/main/java/com/sahu/chinmaya/misc/SerilizationTest.java
@@ -8,41 +8,37 @@ import java.io.Serializable;
 
 public class SerilizationTest implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
-	// Normal variables 
-    int i = 10, j = 20; 
-  
-    // Transient variables 
-    transient int k = 30; 
-  
-    // Use of transient has no impact here 
-    transient static int l = 40; 
-    transient final int m = 50; 
-    
-    final int n= 60;
-  
-    public static void main(String[] args) throws Exception 
-    { 
-        SerilizationTest input = new SerilizationTest(); 
-  
-        // serialization 
-        FileOutputStream fos = new FileOutputStream("abc.txt"); 
-        ObjectOutputStream oos = new ObjectOutputStream(fos); 
-        oos.writeObject(input); 
-  
-        // de-serialization 
-        FileInputStream fis = new FileInputStream("abc.txt"); 
-        ObjectInputStream ois = new ObjectInputStream(fis); 
-        SerilizationTest output = (SerilizationTest)ois.readObject(); 
-        System.out.println("i = " + output.i); 
-        System.out.println("j = " + output.j); 
-        System.out.println("k = " + output.k); 
-        System.out.println("l = " + output.l);   
-        System.out.println("m = " + output.m);
-        System.out.println("n = " + output.n);
-    } 
+	// Normal variables
+	int i = 10, j = 20;
+
+	// Transient variables
+	transient int k = 30;
+
+	// Use of transient has no impact here
+	transient static int l = 40;
+	transient final int m = 50;
+
+	final int n = 60;
+
+	public static void main(String[] args) throws Exception {
+		SerilizationTest input = new SerilizationTest();
+
+		// serialization
+		FileOutputStream fos = new FileOutputStream("abc.txt");
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(input);
+
+		// de-serialization
+		FileInputStream fis = new FileInputStream("abc.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		SerilizationTest output = (SerilizationTest) ois.readObject();
+		System.out.println("i = " + output.i);
+		System.out.println("j = " + output.j);
+		System.out.println("k = " + output.k);
+		System.out.println("l = " + output.l);
+		System.out.println("m = " + output.m);
+		System.out.println("n = " + output.n);
+	}
 }
